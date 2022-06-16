f5adc3a7fb32f73f4428410960a418b20ac1225f
amarouane <amarouane@itsc.uah.edu>
2022-06-16 12:30:06 -0500

    Making some changes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 example command  
-python ./reingest.py -p ./test.csv -c collection -q sqs
+python ./reingest.py -p ./test.csv -c gpmkdmx2ifld
--- reingest.py
+++ reingest.py
@@ -5,131 +5,139 @@ import boto3
 import sys, getopt
 from urllib.parse import urlparse
 
-if os.environ['AWS_PROFILE'] == 'localstack':
+
+AWS_PROFILE = os.environ.get('AWS_PROFILE')
+if not AWS_PROFILE:
+    print("Please define AWS_PROFILE env variable")
+    raise Exception("AWS_PROFILE should be defined")
+
+if  AWS_PROFILE == 'localstack':
     sqs = boto3.client('sqs', region_name=os.environ['AWS_REGION'],
-                        endpoint_url=os.environ['LOCALSTACK_ENDPOINT_URL'])
+                       endpoint_url=os.environ['LOCALSTACK_ENDPOINT_URL'])
+
 else:
     boto3.setup_default_session(profile_name=os.environ['AWS_PROFILE'])
     sqs= boto3.client('sqs')
 
-def generate_payload(s3_uri, collection, size):
+def generate_payload_execution_name(s3_uri, collection, size):
     filename = os.path.basename(s3_uri)
     parsed_uri = urlparse(s3_uri)
-    bucket = parsed_uri.netloc
     path = parsed_uri.path[1:len(parsed_uri.path)-len(filename)-1]
-    payload = { "cumulu_meta": 
+    execution_name = f"{collection}-{filename}-{int(time.time())}"
+    payload = {
+    "cumulus_meta": {
+        "cumulus_version": "11.1.1",
+        "execution_name": execution_name,
+        "message_source": "sfn",
+        "queueExecutionLimits": {
+            "https://sqs.us-west-2.amazonaws.com/349778025075/ghrcw-backgroundJobQueue": 900,
+            "https://sqs.us-west-2.amazonaws.com/349778025075/ghrcw-backgroundProcessing": 5,
+            "https://sqs.us-west-2.amazonaws.com/349778025075/ghrcw-near-real-time": 900,
+            "https://sqs.us-west-2.amazonaws.com/349778025075/ghrcw-ongoing": 900
+        },
+        "state_machine": "arn:aws:states:us-west-2:349778025075:stateMachine:ghrcw-IngestGranule",
+        "system_bucket": "ghrcw-internal",
+        "workflow_start_time": 1655389940553,
+        "parentExecutionArn": "arn:aws:states:us-west-2:349778025075:execution:ghrcw-DiscoverGranules:fefregegeg-4fcaa24d-d71e-48a6-baef-f3bef5c4bdb8",
+        "queueUrl": "https://sqs.us-west-2.amazonaws.com/349778025075/ghrcw-near-real-time"
+    },
+    "exception": None,
+    "meta": {
+        "buckets": {
+            "dashboard": {
+                "name": "ghrcw-dashboard",
+                "type": "public"
+            },
+            "internal": {
+                "name": "ghrcw-internal",
+                "type": "internal"
+            },
+            "orca_default": {
+                "name": "prod-orca-glacier-archive",
+                "type": "orca"
+            },
+            "private": {
+                "name": "ghrcw-private",
+                "type": "private"
+            },
+            "protected": {
+                "name": "ghrcw-protected",
+                "type": "protected"
+            },
+            "public": {
+                "name": "ghrcw-public",
+                "type": "public"
+            },
+            "sharedprivate": {
+                "name": "ghrcw-private",
+                "type": "sharedprivate"
+            }
+        },
+        "cmr": {
+            "clientId": "ghrc_daac",
+            "cmrEnvironment": "OPS",
+            "cmrLimit": 100,
+            "cmrPageSize": 50,
+            "oauthProvider": "launchpad",
+            "passwordSecretName": "ghrcw-message-template-cmr-password20200309200329692700000005",
+            "provider": "GHRC_DAAC",
+            "username": "ghrc_daac"
+        },
+        "collection": {
+      "name": f"{collection}",
+      "version": "1",
+      "dataType": f"{collection}",
+      "process": "metadataextractorActivity",
+      "url_path": f"{collection}__1",
+      "duplicateHandling": "replace",
+      "granuleId": "^Level2_KDMX_.*.(ar2v)$",
+      "granuleIdExtraction": "^((Level2_KDMX_).*)",
+      "reportToEms": True,
+      "sampleFileName": "Level2_KDMX_20130618_0148.ar2v",
+      "meta": {
+        "provider_path": f"{collection}/fieldCampaigns/gpmValidation/ifloods/NEXRAD2/KDMX/data/",
+        "hyrax_processing": "false",
+        "large_dataset": "true",
+        "metadata_extractor": [
+          {
+            "regex": "^Level2_KDMX_(.*).*.(ar2v)$",
+            "module": "netcdf"
+          }
+        ]
+      },
+      "files": [
         {
-            "cumulus_version": "11.1.1",
-            "execution_name": f"{collection}-{filename}-{int(time.time())}",
-            "message_source": "sfn",
-            "queueExecutionLimits": {
-                "https://sqs.us-west-2.amazonaws.com/349778025075/ghrcw-backgroundJobQueue": 900,
-                "https://sqs.us-west-2.amazonaws.com/349778025075/ghrcw-backgroundProcessing": 5,
-                "https://sqs.us-west-2.amazonaws.com/349778025075/ghrcw-near-real-time": 900,
-                "https://sqs.us-west-2.amazonaws.com/349778025075/ghrcw-ongoing": 900
-            },
-            "state_machine": "arn:aws:states:us-west-2:349778025075:stateMachine:ghrcw-IngestGranule",
-            "system_bucket": "ghrcw-internal",
-            "workflow_start_time": None,
-            "parentExecutionArn": "arn:aws:states:us-west-2:349778025075:execution:ghrcw-DiscoverGranules:isslis_v2_nrt-947ed5cb-0d37-4846-83e9-97d5f85c6e3a"
+          "bucket": "public",
+          "regex": "^Level2_KDMX_(.*).*.cmr.(xml|json)$",
+          "sampleFileName": "Level2_KDMX_20130618_0148.ar2v.cmr.xml"
         },
-        "exception": None,
-        "meta": {
-            "buckets": {
-                "dashboard": {
-                    "name": "ghrcw-dashboard",
-                    "type": "public"
-                },
-                "internal": {
-                    "name": "ghrcw-internal",
-                    "type": "internal"
-                },
-                "orca_default": {
-                    "name": "prod-orca-glacier-archive",
-                    "type": "orca"
-                },
-                "private": {
-                    "name": "ghrcw-private",
-                    "type": "private"
-                },
-                "protected": {
-                    "name": "ghrcw-protected",
-                    "type": "protected"
-                },
-                "public": {
-                    "name": "ghrcw-public",
-                    "type": "public"
-                },
-                "sharedprivate": {
-                    "name": "ghrcw-private",
-                    "type": "sharedprivate"
-                }
-            },
-            "cmr": {
-                "clientId": "ghrc_daac",
-                "cmrEnvironment": "OPS",
-                "cmrLimit": 100,
-                "cmrPageSize": 50,
-                "oauthProvider": "launchpad",
-                "passwordSecretName": "ghrcw-message-template-cmr-password20200309200329692700000005",
-                "provider": "GHRC_DAAC",
-                "username": "ghrc_daac"
-            },
-            "collection": {
-            "name":"gpmkdmx2ifld",
-            "version":"1",
-            "dataType":"gpmkdmx2ifld",
-            "process":"metadataextractor",
-            "url_path":"gpmkdmx2ifld__1",
-            "duplicateHandling":"replace",
-            "granuleId":"^Level2_KDMX_.*\\.(ar2v)$",
-            "granuleIdExtraction":"^((Level2_KDMX_).*)",
-            "reportToEms":True,
-            "sampleFileName":"Level2_KDMX_20130618_0148.ar2v",
-            "meta":{
-                "provider_path":"gpmkdmx2ifld/fieldCampaigns/gpmValidation/ifloods/NEXRAD2/KDMX/data/",
-                "hyrax_processing":"false",
-                "large_dataset": "true",
-                "metadata_extractor":[
-                {
-                    "regex":"^Level2_KDMX_(.*).*\\.(ar2v)$",
-                    "module":"netcdf"
-                }
-                ]
-            },
-            "files":[
-                {
-                "bucket":"public",
-                "regex":"^Level2_KDMX_(.*).*\\.cmr.(xml|json)$",
-                "sampleFileName":"Level2_KDMX_20130618_0148.ar2v.cmr.xml"
-                },
-                {
-                "bucket":"protected",
-                "regex":"^Level2_KDMX_(.*).*(ar2v)$",
-                "sampleFileName":"Level2_KDMX_20130618_0148.ar2v"
-                }
-            ]
+        {
+          "bucket": "protected",
+          "regex": "^Level2_KDMX_(.*).*(ar2v)$",
+          "sampleFileName": "Level2_KDMX_20130618_0148.ar2v"
+        }
+      ]
+    },
+        "distribution_endpoint": "https://data.ghrc.earthdata.nasa.gov/",
+        "launchpad": {
+            "api": "https://api.launchpad.nasa.gov/icam/api/sm/v1",
+            "certificate": "launchpad.pfx",
+            "passphraseSecretName": "ghrcw-message-template-launchpad-passphrase20200309200328262000000001"
         },
-            "distribution_endpoint": "https://data.ghrc.earthdata.nasa.gov/",
-            "launchpad": {
-                "api": "https://api.launchpad.nasa.gov/icam/api/sm/v1",
-                "certificate": "launchpad.pfx",
-                "passphraseSecretName": "ghrcw-message-template-launchpad-passphrase20200309200328262000000001"
-            },
-            "provider": {
-                "id": "private_bucket",
-                "globalConnectionLimit": 900,
-                "host": "ghrcw-private",
-                "protocol": "s3",
-                "createdAt": 1592857271267,
-                "updatedAt": 1592857271267
-            },
-            "stack": "ghrcw",
-            "template": "s3://ghrcw-internal/ghrcw/workflow_template.json",
-            "workflow_name": "IngestGranule",
-            "workflow_tasks": {}
+        "provider": {
+            "id": "private_bucket",
+            "globalConnectionLimit": 900,
+            "host": "ghrcw-private",
+            "protocol": "s3",
+            "createdAt": 1592857271267,
+            "updatedAt": 1592857271267
         },
-        "payload": {
+        "stack": "ghrcw",
+        "template": "s3://ghrcw-internal/ghrcw/workflow_template.json",
+        "workflow_name": "IngestGranule",
+        "workflow_tasks": {}
+    },
+    "payload": {
             "granules": [
                 {
                     "granuleId": filename,
@@ -152,9 +160,9 @@ def generate_payload(s3_uri, collection, size):
                 }
             ]
         }
-    }
+}
     
-    return payload
+    return [payload, execution_name]
 
 
 def send_to_sqs(payload, sqs_name):
@@ -170,27 +178,30 @@ def send_to_sqs(payload, sqs_name):
         print(f"{message_body}\n\n")
         
         
-def start_execution(payload):
-    
-    sfn_client = boto3.client('stepfunctions')
-    resoponse = sfn_client.start_execution(
+def start_execution(payload, exec_name, sfn_client):
+    response = sfn_client.start_execution(
         stateMachineArn="arn:aws:states:us-west-2:349778025075:stateMachine:ghrcw-IngestGranule",
-        input= f"""{payload}""")
-    print(resoponse)
+        name=exec_name,
+        input= json.dumps(payload))
+    print(f"Execution run successfully {response['executionArn']}")
 
 
-def reingest(csv_path, collection, sqs_name):
-    
+def reingest(csv_path, collection):
+    sfn_client = boto3.client('stepfunctions')
+    counter = 0
     with open(csv_path, 'r', encoding='utf-8') as in_file:
-        while True:
-            line = in_file.readline()
-            if not line:
-                break
+        line = in_file.readline()
+        while line:
+            counter += 1
             p_line = line.split(',')
             s3_uri, size = p_line[0], p_line[1]
-            payload = generate_payload(s3_uri, collection, size)
-            #send_to_sqs(payload, sqs_name)
-            start_execution(payload)
+            payload, exec_name = generate_payload_execution_name(s3_uri, collection, size)
+            print(f"About to start the execution: {exec_name}")
+            start_execution(payload, exec_name, sfn_client)
+            line = in_file.readline()
+            if counter % 10 == 0:
+                print("Going to sleep for 1 sec")
+                time.sleep(1)
             
 
 
@@ -198,10 +209,10 @@ def main(argv):
     
     csv_path = ''
     collection = ''
-    sqs_name = ''
+
     
     try:
-      opts, args = getopt.getopt(argv, "p:c:q:")
+      opts, _ = getopt.getopt(argv, "p:c:")
     except getopt.GetoptError as err:
       print(f'Error: {err}')
       sys.exit(2)
@@ -211,10 +222,9 @@ def main(argv):
             csv_path = arg
         elif opt == '-c':
             collection = arg
-        elif opt == '-q':
-            sqs_name = arg
+
     
-    reingest(csv_path, collection, sqs_name)
+    reingest(csv_path, collection)
             
 
 if __name__ == '__main__':
